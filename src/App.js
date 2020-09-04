6cb80bb23e14f8e111ecdfa0fd73f2ac86903066
Anunay-Ananad <anunayanand@gmail.com>
2020-09-04 13:39:57 +0530

    Added header component and links to homepage & shoppage

--- src/App.js
+++ src/App.js
@@ -11,10 +11,13 @@ import HomePage from './pages/homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component.jsx';
 
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return ( <
     div >
+    <
+    Header / >
     <
     Switch >
     <
